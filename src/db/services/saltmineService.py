0216b8b15d1503b28c516b44c580b9c17a317a25
Robert Mersiowsky <tecnaps@protonmail.com>
2026-01-14 00:16:27 +0100

    refactoring database 3b

--- src/db/services/saltmineService.py
+++ src/db/services/saltmineService.py
@@ -1,23 +1,39 @@
-from ..repositories import (UserRepository, 
-                            RoomRepository, 
-                            MessageRepository, 
-                            UserInRoomRepository, 
-                            SubRepository)
+from ..repositories import (BitsRepository, 
+                            MessageRepository,
+                            RaidRepository,
+                            RoomRepository,
+                            RoomStateRepository,
+                            SubRepository,
+                            SubgiftRepository,
+                            UserRepository,  
+                            UserInRoomRepository,
+                            UserlistRepository 
+                            )
 
 class SaltmineService:
 
     def __init__(self,
-                 user_repo: UserRepository,
-                 room_repo: RoomRepository,
+                 bits_repo: BitsRepository,
                  message_repo: MessageRepository,
+                 raid_repo: RaidRepository,
+                 room_repo: RoomRepository,
+                 roomstate_repo: RoomStateRepository,
+                 sub_repo: SubRepository,
+                 subgift_repo: SubgiftRepository,
+                 user_repo: UserRepository,
                  userInRoom_repo: UserInRoomRepository,
-                 sub_repo: SubRepository):
+                 userlist_repo: UserlistRepository):
         
-        self.user_repo = user_repo
-        self.room_repo = room_repo
+        self.bits_repo = bits_repo
         self.message_repo = message_repo
-        self.userInRoom_repo = userInRoom_repo
+        self.raid_repo = raid_repo
+        self.room_repo = room_repo
+        self.roomstate_repo = roomstate_repo
         self.sub_repo = sub_repo
+        self.subgift_repo = subgift_repo
+        self.user_repo = user_repo
+        self.userInRoom_repo = userInRoom_repo
+        self.userlist_repo = userlist_repo
 
     def process_message(self, parsed_message: dict) -> None:
         """Process parsed message"""
